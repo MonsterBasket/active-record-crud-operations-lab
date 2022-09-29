c93afc46ed0acfb893f3b413e63fb09974f9a481
MonsterBasket <blazza1@hotmail.com>
2022-09-29 17:38:59 +1000

    done

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 Bundler.require
 
+ActiveRecord::Base.logger.level= 1
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
